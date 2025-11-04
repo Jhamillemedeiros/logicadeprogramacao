970d4c7f92b02dff21f69868c09acb57eb07e17c
Jhamillemedeiros <jhemelli22@gmail.com>
2025-11-03 21:38:03 -0300

    Aula 03/11

--- 00_atividades/atividade_01/src/com/atividade01/app/App.java
+++ 00_atividades/atividade_01/src/com/atividade01/app/App.java
@@ -4,41 +4,37 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       
+       //isntancia a Scanner
         Scanner leia = new Scanner (System.in);
 
-        //variáveis 
+        //declaração de variáveis 
          String nome;
-         String data;
+         String dataNasc;
          String cpf;
          String email;
-         double telefone;
+         String telefone;
 
 
         // entrada de dados
         System.out.println("Informe seu nome:");
         nome= leia.nextLine();
-        System.out.println ("Informe sua data:");
-        data = leia.nextLine();
+        System.out.println ("Informe sua dataNasc:");
+        dataNasc = leia.nextLine();
         System.out.println("Informe sua cpf:");
         cpf = leia.nextLine();
-        System.out.println("Informe seu telefone:");
-        telefone = leia.nextDouble();
-        
-        // limpeza de buffer
-        leia.nextLine();
-
         System.out.println("Informe seu email:");
         email = leia.nextLine ();
-
+        System.out.println("Informe seu telefone:");
+        telefone = leia.nextLine();
+        
          // saida de dados
         System.out.println("Nome: " + nome);
-        System.out.println("Data: " + data);
-        System.out.println("Cpf: " + cpf);
+        System.out.println("Data de nascimento: " + dataNasc);
+        System.out.println("CPF: " + cpf);
         System.out.println("E-mail: " + email);
         System.out.println("Telefone: " + telefone);
 
-        // fechar objeto close
+        // fecha objeto leia
         leia.close();
 
 
--- 00_atividades/atividade_02/src/com/atividade02/app/App.java
+++ 00_atividades/atividade_02/src/com/atividade02/app/App.java
@@ -1,9 +1,36 @@
 package com.atividade02.app;
 
-import util.java.Scanner;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      //instancia Scanner
+        Scanner leia = new Scanner(System.in);
+
+     //equação de 1º grau: a*x + b=0
+
+     // declaração de variavéis
+     double a;
+     double b;
+     double x;
+
+     // entrada doos coeficientes
+        System.out.println("Informe o valor de 'a':");
+        a = leia.nextDouble();
+         System.out.println("Informe o valor de 'b': ");
+        b = leia.nextDouble();
+        
+        //calcule a equação de ! grau a*x + b = 0
+        if (a != 0) {
+            x = -b/a;
+            System.out.println("x = "+ x);
+        }
+        else{
+            System.out.println("Não exixte raiz real.");
+        }
+
+        leia.close();
+
     }
 }
